0b20ed9ce6b3acb052cec6acdafd4b2efb541459
Marina Ivanova <marina0757@gmail.com>
2020-04-26 19:55:20 +0900

    disabling SWPrecacheWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { VueLoaderPlugin } = require('vue-loader')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+// const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 
 module.exports = (env, argv) => ({
   mode: argv && argv.mode || 'development',
@@ -61,14 +61,14 @@ module.exports = (env, argv) => ({
       template: path.resolve(__dirname, 'static', 'index.html'),
       inject: true
     }),
-    new SWPrecacheWebpackPlugin({
-      cacheId: 'todo-vue-pwa',
-      filename: 'service-worker-cache.js',
-      staticFileGlobs: ['dist/**/*.{js,css}', '/'],
-      minify: true,
-      stripPrefix: 'dist/',
-      dontCacheBustUrlsMatching: /\.\w{6}\./
-    }),
+    // new SWPrecacheWebpackPlugin({
+    //   cacheId: 'todo-vue-pwa',
+    //   filename: 'service-worker-cache.js',
+    //   staticFileGlobs: ['dist/**/*.{js,css}', '/'],
+    //   minify: true,
+    //   stripPrefix: 'dist/',
+    //   dontCacheBustUrlsMatching: /\.\w{6}\./
+    // }),
     new CopyWebpackPlugin([{
       from: path.resolve(__dirname, 'static'),
       to: path.resolve(__dirname, 'dist'),
