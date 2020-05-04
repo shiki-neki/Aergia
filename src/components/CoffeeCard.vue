842fbc2ebd75ec480563f3c1c5195e859ce16d84
Marina Ivanova <marina0757@gmail.com>
2020-05-04 11:49:30 +0900

    Coffee card structure

--- src/components/CoffeeCard.vue
+++ src/components/CoffeeCard.vue
@@ -1,9 +1,43 @@
 <template>
-    <div id="coffee-card">
-        <h3>{{ blend }}</h3>
+    <form id="coffee-card">
+        <label>Blend</label>
+        <input type="text" rows="1" v-model="blend" max-length="150">
+        <div class="coffee-card-in" id="aroma">
+            <label>Aroma</label>
+            <input type="range" min="1" max="5" value="3">
+        </div>
+        <div class="coffee-card-in" id="acidity">
+            <label>Acidity</label>
+            <input type="range" min="1" max="5" value="3">
+        </div>
+        <div class="coffee-card-in" id="sweetness">
+            <label>Sweetness</label>
+            <input type="range" min="1" max="5" value="3">
+        </div>
+        <div class="coffee-card-in" id="body">
+            <label>Body</label>
+            <input type="range" min="1" max="5" value="3">
+        </div>
+        <div class="coffee-card-in" id="finish">
+            <label>Finish</label>
+            <input type="range" min="1" max="5" value="3">
+        </div>
+        <div id="overall-notes">
+            <div id="flavor">
+                <label>Flavor</label>
+                <textarea rows="5" v-model="flavor"></textarea>
+            </div>
+            <div id="overall">
+                <label>Overall</label>
+                <textarea rows="5" v-model="overall"></textarea>
+            </div>
+        </div>
+        <div id="rating"></div>
+        <div></div>
         <p> You clicked me {{ count }} times!</p>
-    </div>
+    </form>
 </template>
+
 <script>
 import NewCardButton from './NewCardButton';
 
@@ -11,7 +45,9 @@ import NewCardButton from './NewCardButton';
         props: ['count'],
         data() {
             return {
-                blend: 'Ethiopia',
+                blend: '',
+                flavor: '',
+                overal: ''
             }
         }
     }
